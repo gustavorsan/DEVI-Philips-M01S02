f2f6037be47d94cafd38c5d4b65413e5c1ccb1a6
gustavorsan <gustavorsantos42@gmail.com>
2022-10-14 21:56:27 -0300

    conclusão do ex M1S02E06

--- M1S02E05/js.js
+++ M1S02E05/js.js
@@ -1,8 +1,3 @@
-/*Exiba um prompt solicitando a entrada de um número para saber quantos pares e impares existem de 0 até o número digitado.
-Calcule o número de pares e impares e exiba o resultado utilizando um alert.
-
-Realize o commit do exercício resolvido.*/
-
 let num = parseInt(prompt('Digite um número :'));
 
 let impares = 0;
--- M1S02E06/js.js
+++ M1S02E06/js.js
@@ -1,1 +1,42 @@
+function validarOperacao(operacao){
+  return operacao === '+' || operacao === '-' || operacao === '*' || operacao === '/';
+}
 
+function validarNumero(numero){
+  return isNaN(numero);
+}
+
+let operacao = prompt("Digite uma operação[*, -, /, +]: ");
+while(!validarOperacao(operacao)){
+  operacao = prompt("Digite uma operação válida [*, -, /, +]: ");
+}
+
+let operador1 = prompt("Digite um número: ");
+while(validarNumero(operador1)){
+  operador1 = prompt("Digite um número válido: ");
+}
+
+let operador2 = prompt("Digite um número: ");
+while(validarNumero(operador2)){
+  operador2 = prompt("Digite um número válido: ");
+}
+
+let calculo = 0;
+
+switch(operacao){
+  case '*':
+    calculo = operador1 * operador2;
+  break;
+  case '-': 
+    calculo = operador1 - operador2; 
+  break;
+  case '/': 
+    calculo = operador1 / operador2; 
+  break;
+  case '+': 
+    calculo = operador1 + operador2; 
+  break;
+  default:
+}
+
+alert(`O resultado do seu cálculo é: ${calculo}`)
