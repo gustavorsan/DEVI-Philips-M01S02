3364af934f1ad4a93db0fa9e311e95e1f3af67c1
gustavorsan <gustavorsantos42@gmail.com>
2022-10-15 06:55:57 -0300

    conclusão do ex M1S02E08

--- M1S02E08/js.js
+++ M1S02E08/js.js
@@ -1,3 +1,20 @@
-/**Percorra os números de o a 1000 e exiba no console os números primos e ao final a quantidade de números primos que existem nesse intervalo.
+let quantidadeNumPrimo = 0;
 
-Realize o commit do exercício resolvido. */
+function validarNumeroPrimo(numero){
+  let divisores = 0
+  for(let i = 1; i <= numero; i++){
+    if(numero % i === 0 ){
+      divisores++;
+    }
+  }
+  
+  return divisores === 2;
+}
+
+for(let numero = 0; numero <= 1000; numero++ ){
+  if(validarNumeroPrimo(numero)){
+    quantidadeNumPrimo++;
+  }
+}
+
+alert(`A quantidade de numero primos ente 0 e 1000 é ${quantidadeNumPrimo}`);
